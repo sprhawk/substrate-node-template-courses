f454f22c256660fe270c95b5c26680d1fd3c8123
Yang Hongbo <yanghongbo@iotpi.io>
2021-01-07 23:11:54 +0800

    added more tests as exercises

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -49,4 +49,51 @@ fn revoke_claim_failed_when_claim_is_not_exist() {
     })
 }
 
+#[test]
+fn revoke_claim_failed_when_not_owner() {
+    new_test_ext().execute_with(|| {
+        let claim: Vec<u8> = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        assert_noop!(
+            PoeModule::revoke_claim(Origin::signed(2), claim.clone()),
+            Error::<Test>::NotClaimOwner
+        );
+    })
+}
 
+#[test]
+fn transfer_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim: Vec<u8> = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        assert_ok!(PoeModule::transfer_claim(
+            Origin::signed(1),
+            claim.clone(),
+            2
+        ));
+    })
+}
+
+#[test]
+fn transfer_claim_failed_when_not_owner() {
+    new_test_ext().execute_with(|| {
+        let claim: Vec<u8> = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        assert_noop!(
+            PoeModule::transfer_claim(Origin::signed(2), claim.clone(), 2),
+            Error::<Test>::NotClaimOwner
+        );
+    })
+}
+
+#[test]
+fn transfer_claim_failed_when_claim_is_not_exist() {
+    new_test_ext().execute_with(|| {
+        let claim: Vec<u8> = vec![0, 1];
+
+        assert_noop!(
+            PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2),
+            Error::<Test>::ClaimNotExist
+        );
+    })
+}
