4980b8a26e06278819110a5e8401fce6fcef2935
Yang Hongbo <yanghongbo@iotpi.io>
2021-01-07 22:38:46 +0800

    tests on course

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -1,23 +1,52 @@
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
 
+use super::*;
+
 #[test]
-fn it_works_for_default_value() {
+fn create_claim_works() {
     new_test_ext().execute_with(|| {
-        // Dispatch a signed extrinsic.
-        assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-        // Read pallet storage and assert an expected result.
-        assert_eq!(TemplateModule::something(), Some(42));
-    });
+        let claim: Vec<u8> = vec![0, 1];
+        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+        assert_eq!(
+            Proofs::<Test>::get(&claim),
+            (1, frame_system::Module::<Test>::block_number())
+        );
+    })
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn create_claim_failed_when_claim_already_exists() {
     new_test_ext().execute_with(|| {
-        // Ensure the expected error is thrown when no value is present.
+        let claim: Vec<u8> = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
         assert_noop!(
-            TemplateModule::cause_error(Origin::signed(1)),
-            Error::<Test>::NoneValue
+            PoeModule::create_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ProofAlreadyClaimed,
         );
-    });
+    })
 }
+
+#[test]
+fn revoke_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim: Vec<u8> = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+    })
+}
+
+#[test]
+fn revoke_claim_failed_when_claim_is_not_exist() {
+    new_test_ext().execute_with(|| {
+        let claim: Vec<u8> = vec![0, 1];
+
+        assert_noop!(
+            PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ClaimNotExist
+        );
+    })
+}
+
+
